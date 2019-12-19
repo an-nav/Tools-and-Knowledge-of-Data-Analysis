7475ef3b021af0dc2b8c4ccde8329fd639b648c2
an-nav <45551010+an-nav@users.noreply.github.com>
2019-12-19 12:06:55 +0800

    增加注释

--- NLP/Model/LDA(Latent Dirichlet Allocation)/LDA.py
+++ NLP/Model/LDA(Latent Dirichlet Allocation)/LDA.py
@@ -20,7 +20,16 @@ class LdaApplication:
         tfVector = self.tfVector
         return tfVector
 
-    def train_model(self, n_components=3, learning_offset=10.0, learning_decay=0.7, max_doc_update_iter=100, n_jobs=-1):
+    def train_model(self, n_components, learning_offset=10.0, learning_decay=0.7, max_doc_update_iter=100, n_jobs=-1):
+        '''
+        训练LDA模型
+        :param n_components: LDA的主题参数
+        :param learning_offset:
+        :param learning_decay:
+        :param max_doc_update_iter:
+        :param n_jobs:
+        :return: lda model
+        '''
         ldaModel = LatentDirichletAllocation(n_components=n_components,
                                              learning_decay=learning_decay,
                                              learning_offset=learning_offset,
@@ -32,6 +41,11 @@ class LdaApplication:
         return ldaModel
 
     def grid_search_best_model(self, params):
+        '''
+        网格搜索训练最佳LDA依据极大似然值
+        :param params: 网格搜索参数字典
+        :return: best lda model
+        '''
         ldaModel = LatentDirichletAllocation(n_jobs=-1)
         gridSearchModel = GridSearchCV(ldaModel, param_grid=params, cv=5)
         gridSearchModel.fit(self._get_tf_vector())
@@ -45,6 +59,11 @@ class LdaApplication:
         pass
 
     def get_topic_distribution(self, model):
+        '''
+        展示主题分布
+        :param model: 训练好的LDA模型
+        :return: 词分布的df
+        '''
         ldaOutput = model.transform(self._get_tf_vector())
         topicNames = ["Topic" + str(i) for i in range(model.n_components)]
         docNames = ["Doc" + str(i) for i in range(len(self.corpus))]
@@ -56,6 +75,12 @@ class LdaApplication:
         return dfTopicDistribution
 
     def get_top_n_words(self, model, n_words=10):
+        '''
+        展示每个主题下top_n的词
+        :param model: 训练好的LDA 模型
+        :param n_words: n的数量 默认10
+        :return: 各个主题下top_n词的df
+        '''
         keywords = np.array(self.tfVectorizer.get_feature_names())
         topic_keywords = []
         for topic_weights in model.components_:
@@ -66,12 +91,23 @@ class LdaApplication:
         df_topic_keywords.index = ['Topic ' + str(i) for i in range(df_topic_keywords.shape[0])]
         return df_topic_keywords
 
-    def get_lda_vis(self, model):
+    def get_lda_vis(self, model, path='lda.html'):
+        '''
+        生成一个LDA结果可视化HTML
+        :param path: Html 文件保存路径 默认 lda.html
+        :param model: 需要可视化的LDA模型
+        '''
         panel = pyLDAvis.sklearn.prepare(lda_model=model, vectorizer=self.tfVectorizer, dtm=self.tfVector, mds='tsne')
         # pyLDAvis.show(panel)
-        pyLDAvis.save_html(panel, 'lda.html')
+        pyLDAvis.save_html(panel, path)
 
     def predict(self, model, corpus):
+        '''
+        聚类新的文本并给出聚类结果
+        :param model: 训练好的LDA模型
+        :param corpus: 需要预测的语料
+        :return: 语料对应聚类结果的矩阵
+        '''
         tfVector = self.tfVectorizer.transform(corpus)
         ldaOutpu = model.transform(tfVector)
         results = np.argmax(ldaOutpu, axis=1)
@@ -97,8 +133,3 @@ if __name__ == '__main__':
     top_n_words = lda.get_top_n_words(model)
     print(top_n_words)
     lda.get_lda_vis(model)
-
-    #肉眼看来盲标注
-    #分词白名单
-    #增加词库
-    #增加停用词
